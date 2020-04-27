11a55e4a07979a26c3fa1110d4ef4c368a17abde
CeeDeeBee <cdbarnes98@gmail.com>
2020-04-27 18:02:30 -0400

    Added user_comments table and updated readme

--- data/migrations/20200427151846_initialize.js
+++ data/migrations/20200427151846_initialize.js
@@ -1,11 +1,24 @@
 exports.up = function (knex) {
-	return knex.schema.createTable("users", (users) => {
-		users.increments();
-		users.string("username", 255).notNullable().unique();
-		users.string("password", 255).notNullable();
-	});
+	return knex.schema
+		.createTable("users", (users) => {
+			users.increments();
+			users.string("username", 255).notNullable().unique();
+			users.string("password", 255).notNullable();
+		})
+		.createTable("user_comments", (userComments) => {
+			userComments.increments();
+			userComments
+				.integer("user_id")
+				.notNullable()
+				.references("users.id")
+				.onDelete("RESTRICT")
+				.onUpdate("CASCADE");
+			userComments.integer("comment_id").notNullable();
+		});
 };
 
 exports.down = function (knex) {
-	return knex.schema.dropTableIfExists("users");
+	return knex.schema
+		.dropTableIfExists("users")
+		.dropTableIfExists("user_comments");
 };
