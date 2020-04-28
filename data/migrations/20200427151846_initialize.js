16497b4e6dd1b96ab59b498154402b8ebef5ac3d
CeeDeeBee <cdbarnes98@gmail.com>
2020-04-28 13:09:26 -0400

    Added comments table and added future endpoints to readme

--- data/migrations/20200427151846_initialize.js
+++ data/migrations/20200427151846_initialize.js
@@ -5,6 +5,12 @@ exports.up = function (knex) {
 			users.string("username", 255).notNullable().unique();
 			users.string("password", 255).notNullable();
 		})
+		.createTable("comments", (comments) => {
+			comments.integer("id").notNullable().unique();
+			comments.string("user", 255).notNullable();
+			comments.text("comment").notNullable();
+			comments.integer("sentiment").notNullable();
+		})
 		.createTable("user_comments", (userComments) => {
 			userComments.increments();
 			userComments
@@ -13,12 +19,18 @@ exports.up = function (knex) {
 				.references("users.id")
 				.onDelete("RESTRICT")
 				.onUpdate("CASCADE");
-			userComments.integer("comment_id").notNullable();
+			userComments
+				.integer("comment_id")
+				.notNullable()
+				.references("comments.id")
+				.onDelete("RESTRICT")
+				.onUpdate("CASCADE");
 		});
 };
 
 exports.down = function (knex) {
 	return knex.schema
-		.dropTableIfExists("users")
-		.dropTableIfExists("user_comments");
+		.dropTableIfExists("user_comments")
+		.dropTableIfExists("comments")
+		.dropTableIfExists("users");
 };
